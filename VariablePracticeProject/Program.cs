b48efea35263d697de3d34f16727a1b8746a41c0
jkylecarpentry@gmail.com <jkylecarpentry@gmail.com>
2019-08-10 11:49:17 -0400

    added a division calculator

--- VariablePracticeProject/Program.cs
+++ VariablePracticeProject/Program.cs
@@ -10,13 +10,20 @@ namespace VariablePracticeProject
             int num2;
 
             Console.Write($"Type a number for num1: ");
-            num1 = Convert.ToInt32 (Console.ReadLine());
+            num1 = Convert.ToInt32(Console.ReadLine());
             Console.Write($"Type a number for num2: ");
             num2 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("The result is " + num1 * num2);
+        
+        
+            int num10;  //declare the variable
+            int num20;
 
-
-
+            Console.Write($"Type a number for num1: ");
+            num10 = Convert.ToInt32(Console.ReadLine());
+            Console.Write($"Type a number for num2: ");
+            num20 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("num1 divided by num2 is: " + num10 / num20);
         }
     }
 }
