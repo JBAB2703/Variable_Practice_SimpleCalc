d653c020fbf071f86ddce2e9635e7c7fcd53d6ee
jkylecarpentry@gmail.com <jkylecarpentry@gmail.com>
2019-08-10 11:42:14 -0400

    Add project files.

---
+++ VariablePracticeProject/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace VariablePracticeProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;  //declare the variable
+            int num2;
+
+            Console.Write($"Type a number for num1: ");
+            num1 = Convert.ToInt32 (Console.ReadLine());
+            Console.Write($"Type a number for num2: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The result is " + num1 * num2);
+
+
+
+        }
+    }
+}
