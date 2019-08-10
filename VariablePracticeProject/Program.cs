213e5a36157fe9611cccd7a50e1bce995288d38a
jkylecarpentry@gmail.com <jkylecarpentry@gmail.com>
2019-08-10 12:35:01 -0400

    added changes to calculators

--- VariablePracticeProject/Program.cs
+++ VariablePracticeProject/Program.cs
@@ -9,19 +9,26 @@ namespace VariablePracticeProject
             int num1;  //declare the variable
             int num2;
 
-            Console.Write($"Type a number for num1: ");
+            Console.Write("Multiplication Calculator\n");
+            Console.WriteLine();
+            Console.Write($"Please enter a number for num1: ");
             num1 = Convert.ToInt32(Console.ReadLine());
-            Console.Write($"Type a number for num2: ");
+            Console.Write($"Please enter a second number for num2: ");
             num2 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("The result is " + num1 * num2);
-        
-        
-            int num10;  //declare the variable
-            int num20;
 
-            Console.Write($"Type a number for num1: ");
+
+            Console.WriteLine();
+        
+        
+            double num10;  //declare the variable
+            double num20;
+
+            Console.Write("Dividing Calculator\n");
+            Console.WriteLine();
+            Console.Write($"Please enter a number for num1: ");
             num10 = Convert.ToInt32(Console.ReadLine());
-            Console.Write($"Type a number for num2: ");
+            Console.Write($"Please enter a second number for num2: ");
             num20 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("num1 divided by num2 is: " + num10 / num20);
         }
